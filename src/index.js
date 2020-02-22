896728d0dd89c55575c3527c7803bf56577b066a
olechkaS7 <olg.iurist@yandex.ru>
2020-02-22 17:29:35 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const abs = Math.abs(n);
+    const str = String(abs);
+    const arr = str.split('');
+    arr.reverse();
+    const reverseStr = arr.join('');
+    const number = parseInt(reverseStr, 10);
+
+    return number;
 }
